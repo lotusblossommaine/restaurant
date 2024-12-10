365ba1053c66260e9a923040679339a8aa586230
Tiffany Lam <tclam05@gmail.com>
2024-12-09 18:46:34 -0800

    position nav

--- src/App.js
+++ src/App.js
@@ -13,9 +13,11 @@ function App() {
       <div style={{ backgroundImage: `url(${image})` }} className="backgroundImage">
         <div className="header">
           <div className="headerLabel"><h1>Lotus Blossom</h1></div>
-          <div className="nav">
-            <div className={classnames("navItem", { isSelected: !isTakeout })} onClick={() => { setIsTakeout(false) }}>Dine-in Menu</div>
-            <div className={classnames("navItem", { isSelected: isTakeout })} onClick={() => { setIsTakeout(true) }}>Takeout Menu</div>
+          <div className="navContainer">
+            <div className="nav">
+              <div className={classnames("navItem", { isSelected: !isTakeout })} onClick={() => { setIsTakeout(false) }}>Dine-in Menu</div>
+              <div className={classnames("navItem", { isSelected: isTakeout })} onClick={() => { setIsTakeout(true) }}>Takeout Menu</div>
+            </div>
           </div>
         </div>
         <div className="backgroundOverlay">
